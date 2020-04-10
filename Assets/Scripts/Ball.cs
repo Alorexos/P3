f991e5afaaa80b84b15717615f18804801391a2e
Wojciech Cieslinski <wojciech.cieslinski@hotmail.com>
2020-04-10 16:05:49 +0100

    Ball force and collision
    Added basic force to ball on RMB click, and added pyhsics to it so it bouces from the obstacles. Done some initial setup on camera rotation.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public GameObject goCamera;
+    public float fForce = 0.0f;
+    
+    private Rigidbody rbBall;
+    private float fCameraAngle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbBall = GetComponent<Rigidbody>();
+        fCameraAngle = goCamera.transform.rotation.eulerAngles.x;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(fCameraAngle.ToString());
+            rbBall.AddForce(Quaternion.Euler(-fCameraAngle,0,0) * goCamera.transform.forward * fForce, ForceMode.Impulse);
+        }
+
+    }
+}
